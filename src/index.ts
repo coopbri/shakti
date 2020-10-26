3fda83b5c2ebd103a566daef900d7959045ee4f1
brian <brian@brian-cooper.com>
2020-10-26 16:44:39 -0500

    expose breakpoints to public API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Main entry point barrel; represents public API
 
+// constants
+import breakpoints from "./constants/breakpoints";
+
+// components
 import { View, Flex, Grid, Row, Col } from "./components/Layout/Layout";
 import { Text } from "./components/Text/Text";
 
-export { View, Flex, Grid, Row, Col, Text };
+export { breakpoints, View, Flex, Grid, Row, Col, Text };
