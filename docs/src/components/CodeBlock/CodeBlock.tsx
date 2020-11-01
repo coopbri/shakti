583ce636b385b8226e7d07ae1e2a6f3afd16d2e5
Brian <20056195+coopbri@users.noreply.github.com>
2020-11-01 20:23:55 +0000

    Dev (#8)
    * added repository information to package.json
    
    * fixed library name in docs import statements
    
    * added live code edit and static render support
    
    * added sandbox
    
    * added static render to layout page, updated formatting on recipes page
    
    * Docs (#7)
    
    * fixed library name in docs import statements
    
    * added live code edit and static render support
    
    * added sandbox
    
    * added static render to layout page, updated formatting on recipes page
    
    * added window check to avoid SSR error

--- docs/src/components/CodeBlock/CodeBlock.tsx
+++ docs/src/components/CodeBlock/CodeBlock.tsx
@@ -1,16 +1,76 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { mdx } from "@mdx-js/react";
 import prismTheme from "prism-react-renderer/themes/nightOwl";
-import { View } from "shakti-lib";
+import {
+  Grid,
+  Row,
+  Col,
+  View,
+  Flex,
+  Text,
+  breakpoints,
+  useWindowQuery,
+} from "shakti-lib";
+
+interface IProps {
+  children?: any;
+}
+
+// components allowed in code blocks (can be used without import)
+const scope = {
+  mdx,
+  Grid,
+  Row,
+  Col,
+  View,
+  Flex,
+  Text,
+  breakpoints,
+  useWindowQuery,
+};
 
 /**
  * Code block for rendering from MDX source
  * @param children code to be lexed
  */
-const CodeBlock = ({ children }) => {
+const CodeBlock = ({ children }: IProps) => {
   const className = children.props.className || "";
   const matches = className.match(/language-(?<lang>.*)/);
 
+  // live editor (dynamic)
+  if (children.props.live) {
+    return (
+      <LiveProvider
+        code={children.props.children.trim()}
+        transformCode={(code) => "/** @jsx mdx */" + code}
+        scope={scope}
+        theme={prismTheme}
+      >
+        <LiveEditor />
+        <LivePreview />
+        <LiveError />
+      </LiveProvider>
+    );
+  }
+
+  // rendered output (static)
+  if (children.props.render) {
+    return (
+      <LiveProvider
+        code={children.props.children.trim()}
+        transformCode={(code) => "/** @jsx mdx */" + code}
+        scope={scope}
+        theme={prismTheme}
+        noInline={true}
+      >
+        <LivePreview />
+      </LiveProvider>
+    );
+  }
+
+  // standard output: tokenized and static
   return (
     <Highlight
       // default Prism props
