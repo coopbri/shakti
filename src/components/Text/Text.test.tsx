20791726ca94cccbee819724cc18a0b0a87f58e5
Brian Cooper <brian@brian-cooper.com>
2020-11-26 00:22:33 -0600

    added scoped shortcut Text unit test

--- src/components/Text/Text.test.tsx
+++ src/components/Text/Text.test.tsx
@@ -11,6 +11,13 @@ describe("Text", () => {
       </Text>,
     );
 
+  const scopedRenderComponent = () =>
+    render(
+      <Text size={24} weight={700} i s>
+        Test text
+      </Text>,
+    );
+
   test("renders with correct text", () => {
     renderComponent();
     expect(screen.getByText("Test text")).toBeInTheDocument();
@@ -20,5 +27,15 @@ describe("Text", () => {
     renderComponent();
     expect(screen.getByText("Test text")).toHaveStyle("margin-left: 4px;");
     expect(screen.getByText("Test text")).toHaveStyle("color: green;");
+  });
+
+  test("has access to exclusive shortcut props", () => {
+    scopedRenderComponent();
+    expect(screen.getByText("Test text")).toHaveStyle("font-size: 24px;");
+    expect(screen.getByText("Test text")).toHaveStyle("font-weight: 700;");
+    expect(screen.getByText("Test text")).toHaveStyle("font-style: italic;");
+    expect(screen.getByText("Test text")).toHaveStyle(
+      "text-decoration: line-through;",
+    );
   });
 });
